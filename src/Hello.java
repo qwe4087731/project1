813f1daf3f03a7fabb8eb76ad2c4f43b6d10edc5
qz <466284796@qq.com>
2018-10-17 13:52:36 +0800

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+/**
+ * @author shkstart
+ * @create 2018-10-16 16:49
+ */
+public class Hello {
+    public static void main(String[] args) {
+        //大门航
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("helloworld!1323!");
+    }
+
+    public void hello() {
+        System.out.println("werewr");
+        ArrayList list = new ArrayList();
+        list.add();
+        new Date()
+        /*sdfds*/
+    }
+}
